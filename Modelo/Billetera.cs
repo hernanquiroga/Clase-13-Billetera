40f8c2abcf1a3c12b34b2cfde9ea6633d5d56953
Hernan Quiroga <quiroga.hernan@gmail.com>
2022-06-20 19:57:05 -0300

    Agregar archivos de proyecto.

---
+++ Modelo/Billetera.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_13_Billetera.Modelo
+{
+    internal class Billetera
+    {
+        // Propiedades
+        public int billetesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+
+        public int BilletesDe50 { get; set; }
+
+        public int BilletesDe100 { get; set; }
+
+        public int BilletesDe200 { get; set; }
+
+        public int BilletesDe500 { get; set; }
+
+        public int BilletesDe1000 { get; set; }
+
+
+        // Constructor 
+        public Billetera(int billetesDe10, int billetesDe20, int billetesDe50, int billetesDe100, int billetesDe200, int billetesDe500, int billetesDe1000)
+        {
+            this.billetesDe10 = billetesDe10; // this. se utiliza para hacer referencia a la propiedad de la clase para no tener que escribir con mayusculas o diferenciar de alguna manera
+            BilletesDe20 = billetesDe20;
+            BilletesDe50 = billetesDe50;
+            BilletesDe100 = billetesDe100;
+            BilletesDe200 = billetesDe200;
+            BilletesDe500 = billetesDe500;
+            BilletesDe1000 = billetesDe1000;
+        }
+
+        // Métodos / Funciones
+        public decimal Total()      // Sumo total de billetes y sus valores
+        {
+            return (billetesDe10 * 10) + (BilletesDe20 * 20) + (BilletesDe50 * 50) + (BilletesDe100 * 100) + (BilletesDe200 * 200) + (BilletesDe500 * 500) + (BilletesDe1000 * 1000);
+        }
+
+        public decimal BorrarBilleteras() // Borro el total de billetes
+        {
+            return (billetesDe10 = 0) + (BilletesDe20 = 0) + (BilletesDe50 = 0) + (BilletesDe100 = 0) + (BilletesDe200 = 0) + (BilletesDe500 = 0) + (BilletesDe1000 = 0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+//BilletesDe10
+//BilletesDe20
+//BilletesDe50
+//BilletesDe100
+//BilletesDe200
+//BilletesDe500
+//BilletesDe1000
+
+//Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total en la billetera teniendo en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+//Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que devuelva una nueva Billetera con la suma o combinacion del dinero de ambas billeteras.  
+
+//Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+
+using Clase_13_Billetera.Modelo;
+
+var billetera1 = new Billetera(2,4,8,9,4,3,1); // Instanciado desde el constructor
+
+var billetera2 = new Billetera(5,8,9,6,7,8,1);
+
+
+Console.Write("Billetera 1 suma :");
+Console.WriteLine("$" + billetera1.Total() + " Pesos");
+
+Console.Write("Billetera 2 suma :");
+Console.WriteLine("$" + billetera2.Total() + " Pesos");
+
+var billetera3 = billetera1.Total() + billetera2.Total();
+
+Console.Write("Billetera 3 suma :");
+Console.WriteLine(billetera3);
+
+Console.WriteLine("Ahora billetera 1 tiene :" + billetera1.BorrarBilleteras());
+Console.WriteLine("Ahora billetera 2 tiene :" + billetera2.BorrarBilleteras());
+
+
+
+
+
+
+
+
+
+
